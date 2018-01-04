412cc9498c0d54442ddf5090b99ab42b1efe9f0c
Fernando W. Bona <bonafernando@gmail.com>
2018-01-04 21:46:09 -0200

    Friendship Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :registrations => "user/registrations" }
+  resources :users, only: [:show]
+  resources :friendships
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
