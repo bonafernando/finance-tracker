d0640b1f841014e9c9e7d64fc4b3f863bceaa648
Fernando W. Bona <bonafernando@gmail.com>
2018-01-05 12:20:30 -0200

    Users Search Display

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :user_stocks, only: [:create,:destroy]
   get 'my_friends', to: 'users#my_friends'
   get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
