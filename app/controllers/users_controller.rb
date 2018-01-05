420ee5b7cea2ade9ff1d390a5177de3c7781043b
Fernando W. Bona <bonafernando@gmail.com>
2018-01-05 15:59:00 -0200

    Friends Show Action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,20 @@ class UsersController < ApplicationController
     end
     render partial: 'friends/result'
   end
+
+  def add_friend
+    @friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: @friend.id)
+    if current_user.save
+      flash[:notice] = "Friend was successfully added"
+    else
+      flash[:danger] = "There was something wrong with the friend request"
+    end
+    redirect_to my_friends_path
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @user_stocks = @user.stocks
+  end
 end
